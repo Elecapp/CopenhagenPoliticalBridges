e1155a47440e5e50c5948082cbf2b75441c87663
Elecapp <eleonora.cappuccio@outlook.it>
2019-02-21 14:16:41 +0100

    about.structure

---
+++ js/main.js
@@ -0,0 +1,23 @@
+
+/*** AJAX PER IL LINK ***/
+
+$("nav a").on("click", function(event){
+  event.preventDefault()
+
+  const href = $(this).attr("href")
+
+  window.history.pushState(null, null, href)
+
+  $.ajax({
+    url: href,
+    success: function(data) {
+      $("main").fadeOut(400, function(){
+        const newPage = $(data).filter("main").html()
+
+        $("main").html(newPage)
+
+        $("main").fadeIn(400)
+      })
+    }
+  })
+})
