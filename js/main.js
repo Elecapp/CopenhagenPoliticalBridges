9628931e26b23bfd2a087f1076d91ccfe9dfabb9
Elecapp <eleonora.cappuccio@outlook.it>
2019-02-21 15:40:21 +0100

    3

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 
 /*** AJAX PER IL LINK ***/
 
-/*$("nav a").on("click", function(event){
+$("nav a").on("click", function(event){
   event.preventDefault()
 
   const href = $(this).attr("href")
@@ -20,4 +20,38 @@
       })
     }
   })
-})*/
+})
+
+
+
+
+$(document).ready(function () {
+
+    $("#sidebar").mCustomScrollbar({
+         theme: "minimal"
+    });
+
+    $('#sidebarCollapse').on('click', function () {
+        $('#sidebar').toggleClass('active');
+    });
+
+});
+
+
+$(document).ready(function () {
+
+    $("#sidebar").mCustomScrollbar({
+         theme: "minimal"
+    });
+
+    $('#sidebarCollapse').on('click', function () {
+        // open or close navbar
+        $('#sidebar').toggleClass('active');
+        // close dropdowns
+        $('.collapse.in').toggleClass('in');
+        // and also adjust aria-expanded attributes we use for the open/closed arrows
+        // in our CSS
+        $('a[aria-expanded=true]').attr('aria-expanded', 'false');
+    });
+
+});
