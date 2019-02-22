472fd5a86599524c17dbbcd7fd9b9a54bdbc3f4d
Elecapp <eleonora.cappuccio@outlook.it>
2019-02-22 09:41:50 +0100

    cococo

--- js/main.js
+++ js/main.js
@@ -23,6 +23,16 @@ let map1 = document.getElementById("TYPE");
 let map2 = document.getElementById("TOPIC");
 let map3 = document.getElementById("FOOD");
 let map4 = document.getElementById("IMAGES");
+let map5 = document.getElementById("AREAS");
+let map6 = document.getElementById("SERVICE");
+
+let testo1 = document.getElementById("TESTOTYPE");
+let testo2 = document.getElementById("TESTOTOPIC");
+let testo3 = document.getElementById("TESTOFOOD");
+let testo4 = document.getElementById("TESTOIMAGES");
+let testo5 = document.getElementById("TESTOAREA");
+let testo6 = document.getElementById("TESTOSERVICE");
+
 
 function toggleTYPE() {
     map0.classList.add("is-hidden");
@@ -30,7 +40,18 @@ function toggleTYPE() {
     map2.classList.add("is-hidden");
     map3.classList.add("is-hidden");
     map4.classList.add("is-hidden");
+    map5.classList.add("is-hidden");
+    map6.classList.add("is-hidden");
     map1.src = "assets/map_TYPE.png";
+
+    testo1.classList.remove("is-hidden");
+    testo2.classList.add("is-hidden");
+    testo3.classList.add("is-hidden");
+    testo4.classList.add("is-hidden");
+    testo5.classList.add("is-hidden");
+    testo6.classList.add("is-hidden");
+
+
 };
 
 function toggleTOPIC() {
@@ -39,6 +60,8 @@ function toggleTOPIC() {
   map2.classList.remove("is-hidden");
   map3.classList.add("is-hidden");
   map4.classList.add("is-hidden");
+  map5.classList.add("is-hidden");
+  map6.classList.add("is-hidden");
     map2.src = "assets/map_TOPIC.png";
 };
 
@@ -48,6 +71,8 @@ function toggleFOOD() {
   map2.classList.add("is-hidden");
   map3.classList.remove("is-hidden");
   map4.classList.add("is-hidden");
+  map5.classList.add("is-hidden");
+  map6.classList.add("is-hidden");
     map3.src = "assets/map_FOOD.png";
 };
 
@@ -57,5 +82,29 @@ function toggleIMAGES() {
   map2.classList.add("is-hidden");
   map3.classList.add("is-hidden");
   map4.classList.remove("is-hidden");
+  map5.classList.add("is-hidden");
+  map6.classList.add("is-hidden");
     map4.src = "assets/map_IMAGES.png";
 };
+function toggleAREA() {
+  map0.classList.add("is-hidden");
+  map1.classList.add("is-hidden");
+  map2.classList.add("is-hidden");
+  map3.classList.add("is-hidden");
+  map4.classList.add("is-hidden");
+  map5.classList.remove("is-hidden");
+  map6.classList.add("is-hidden");
+    map5.src="assets/map_AREA.png";
+
+}
+function toggleSERVICE() {
+  map0.classList.add("is-hidden");
+  map1.classList.add("is-hidden");
+  map2.classList.add("is-hidden");
+  map3.classList.add("is-hidden");
+  map4.classList.add("is-hidden");
+  map5.classList.add("is-hidden");
+  map6.classList.remove("is-hidden");
+    map6.src="assets/map_AREA.png";
+
+}
