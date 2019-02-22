29960affac9bf65698b9ac0caba9c173901bea3b
Elecapp <eleonora.cappuccio@outlook.it>
2019-02-22 09:58:44 +0100

    6

--- js/main.js
+++ js/main.js
@@ -1,21 +1,4 @@
-/*** AJAX PER IL LINK
-$(document).ready(function() {
-  $("nav a").on("click", function(event) {
-    event.preventDefault()
-    const href = $(this).attr("href")
-    window.history.pushState(null, null, href)
-    $.ajax({
-      url: href,
-      success: function(data) {
-        $("main").fadeOut(400, function() {
-          const newPage = $(data).filter("main").html()
-          $("main").html(newPage)
-          $("main").fadeIn(400)
-        })
-      }
-    })
-  });
-});***/
+
 
 /*** MAPS BUTTONS ***/
 
